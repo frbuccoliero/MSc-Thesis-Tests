3001a67d8c533bdbfa6362679d0815b441c57f9c
Francesco Buccoliero <fr.buccoliero@gmail.com>
2025-03-13 11:32:34 +0100

    Added script to generate CVAT labels automatically

---
+++ template_matching/gen_labels_CVAT.py
@@ -0,0 +1,41 @@
+# This script generates and saves to a file the labels for the CVAT annotation tool
+# Labels are geneerated from the files in the 'templates' folder
+# The type is "rectangle" to have bounding boxes
+
+'''[
+  {
+    "name": "<filename>",
+    "type": "rectangle",
+    "attributes": []
+  }
+]
+'''
+
+import os
+import json
+
+# Path to the templates folder
+templates_folder = 'templates'
+templates_folder = os.path.join(os.path.dirname(__file__), templates_folder)
+
+# Path to the labels file
+labels_file = 'CVAT_labels.json'
+labels_file = os.path.join(os.path.dirname(__file__), labels_file)
+
+# Get the list of files in the templates folder
+files = os.listdir(templates_folder)
+
+# Create the labels list
+labels = []
+for file in files:
+    labels.append({
+        "name": file,
+        "type": "rectangle",
+        "attributes": []
+    })
+
+# Save the labels to the file
+with open(labels_file, 'w') as f:
+    json.dump(labels, f, indent=2)
+
+print(f'Labels saved to {labels_file}')
